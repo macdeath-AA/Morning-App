83210b2e4228b41bb16ea0e36445709cc4e29834
aditi <me20btech11001@iith.ac.in>
2025-01-21 22:02:19 -0500

    user profile header and welcome

--- app/index.js
+++ app/index.js
@@ -1,10 +1,32 @@
-import {View, Text} from 'react-native';
+import {View, ScrollView, SafeAreaView} from 'react-native';
+import { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, icons, images, SIZES} from '../constants';
+import { ScreenHeaderBtn, Welcome} from '../components';
+
 
 const Home = () => {
+    const router = useRouter();
+
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen
+            options={{headerStyle: {backgroundColor: COLORS.lightWhite},
+            headerRight: () => (
+                <ScreenHeaderBtn iconUrl = {icons.profile} dimension = "100%"/>
+            ),
+            headerTitle: ""
+            }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator ={false}>
+                <View style= {{flex:1, padding: SIZES.medium}}>
+                <Welcome/>
+                </View>
+
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
