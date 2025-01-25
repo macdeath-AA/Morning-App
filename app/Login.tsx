5be9b3f6bd2d394f21351e27e216768a68ded043
aditi <me20btech11001@iith.ac.in>
2025-01-25 18:15:50 -0500

    added basic email login w firebase

---
+++ app/Login.tsx
@@ -0,0 +1,78 @@
+import React, {useState} from "react";
+import {View, Text, TextInput, Button, StyleSheet, Alert} from 'react-native';
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+import { FIREBASE_AUTH } from "../firebaseConfig";
+// import stylfrom "../styles/search";
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleLogin = async () => {
+        setLoading(true);
+        try {
+            await signInWithEmailAndPassword(FIREBASE_AUTH, email, password);
+        } catch (error) {
+            Alert.alert('Login Error', error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <View style = {Loginstyles.container}>
+            <Text>
+                Login
+            </Text>
+            <TextInput
+                style = {Loginstyles.input}
+                placeholder= "Email"
+                value= {email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+            />
+
+            <TextInput
+                style = {Loginstyles.input}
+                placeholder="Password"
+                value= {password}
+                onChangeText={setPassword}
+                secureTextEntry
+            />
+            <Button
+                title= {loading ? "Loggin in..." : "Login"}
+                onPress={handleLogin}
+                disabled = {loading}           
+            />
+
+        </View>
+    );
+};
+
+const Loginstyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        backgroundColor: '#fff',
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 16,
+    },
+    input: {
+        width: '100%',
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        marginBottom: 16,
+    },
+});
+
+export default Login;
+
--- app/index.js
+++ app/index.js
@@ -1,15 +1,40 @@
 import { View, ScrollView, SafeAreaView } from 'react-native';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Stack, useRouter } from 'expo-router';
-
 import { COLORS, icons, images, SIZES } from '../constants';
 import { ScreenHeaderBtn, Welcome } from '../components';
 import NewsOption from '../components/home/news/NewsOption';
 import NewsPage from '.';
+import { onAuthStateChanged } from 'firebase/auth';
+import { FIREBASE_AUTH } from '../firebaseConfig';
+import Login from './Login';
+import { ActivityIndicator } from 'react-native';
 
 
 const Home = () => {
     const router = useRouter();
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect( () => {
+        const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return unsubscribe;
+    },[]);
+
+    if (loading) {
+        return (
+            <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <ActivityIndicator size="large" color={COLORS.primary} />
+            </SafeAreaView>
+        );
+    }
+
+    if (!user){
+        return <Login/>;
+    }
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -23,14 +48,11 @@ const Home = () => {
                 }}
             />
 
-            {/* <ScrollView showsVerticalScrollIndicator ={false}> */}
             <View style={{ flex: 1, padding: SIZES.medium }}>
                 <Welcome />
                 <NewsOption />
-                {/* <NewsPage />/ */}
             </View>
 
-            {/* </ScrollView> */}
         </SafeAreaView>
     )
 }
